33b9a67150f677ffd1c4ecf9fa5538eaf043aae9
gibeom <ejsjdj@naver.com>
2025-07-06 03:38:38 +0900

    132page

--- Spring Boot/jpa/src/main/java/com/springboot/jpa/data/dto/ProductResponseDto.java
+++ Spring Boot/jpa/src/main/java/com/springboot/jpa/data/dto/ProductResponseDto.java
@@ -7,6 +7,8 @@ public class ProductResponseDto {
     private int price;
     private int stock;
 
+    public ProductResponseDto() {}
+
     public ProductResponseDto(Long number, String name, int price, int stock) {
         this.number = number;
         this.name = name;
@@ -14,9 +16,33 @@ public class ProductResponseDto {
         this.stock = stock;
     }
 
-    public Long getNumber() {
-        return number;
+    public Long getNumber() { return number; }
+
+    public void setNumber(Long number) {
+        this.number = number;
     }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
